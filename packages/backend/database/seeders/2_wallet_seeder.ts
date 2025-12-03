60921620dc7f72b93ef777e125fe835c8f03379f
lfsc09 <lfsc09@gmail.com>
2025-12-03 13:57:44 -0300

    refactor[backend]: added more data in seeder

--- packages/backend/database/seeders/2_wallet_seeder.ts
+++ packages/backend/database/seeders/2_wallet_seeder.ts
@@ -1,24 +1,24 @@
-import { BaseSeeder } from '@adonisjs/lucid/seeders';
-import Big from 'big.js';
-import { DateTime } from 'luxon';
-import { WalletFactory } from '#database/factories/investment_wallet_factory';
-import { UserFactory } from '#database/factories/user_factory';
-import Wallet from '#models/investment/wallet';
-import User from '#models/user/user';
-import { acceptedCurrencyCodes } from '../../app/core/types/investment/currencies.js';
-import { UserRole } from '../../app/core/types/user/user_roles.js';
+import { BaseSeeder } from '@adonisjs/lucid/seeders'
+import Big from 'big.js'
+import { DateTime } from 'luxon'
+import { WalletFactory } from '#database/factories/investment_wallet_factory'
+import { UserFactory } from '#database/factories/user_factory'
+import Wallet from '#models/investment/wallet'
+import User from '#models/user/user'
+import { acceptedCurrencyCodes } from '../../app/core/types/investment/currencies.js'
+import { UserRole } from '../../app/core/types/user/user_roles.js'
 
 export default class extends BaseSeeder {
   async run() {
-    await Wallet.query().delete();
+    await Wallet.query().delete()
 
-    let userId = (await User.findBy('email', 'admin@gmail.com'))?.id;
+    let userId = (await User.findBy('email', 'admin@gmail.com'))?.id
     if (!userId) {
       userId = (
         await UserFactory.merge([
           { email: 'admin@gmail.com', password: '12345678', role: UserRole.ADMIN },
         ]).create()
-      ).id;
+      ).id
     }
 
     // BRL wallet
@@ -63,6 +63,27 @@ export default class extends BaseSeeder {
       .with('assetBrlPublicBonds', 2, (pb) =>
         pb.with('assetBrlPublicBondBuys', 2).with('assetBrlPublicBondSells', 1),
       )
-      .create();
+      .create()
+
+    await WalletFactory.merge({
+      userId,
+      currencyCode: acceptedCurrencyCodes.find((c) => c === 'BRL'),
+    })
+      .with('movements', 1, (m) =>
+        m
+          .merge({
+            dateUtc: DateTime.fromSQL('2025-01-01'),
+            originAmount: new Big(175000),
+            movementType: 'deposit',
+            originCurrencyCode: 'BRL',
+            resultCurrencyCode: 'BRL',
+          })
+          .apply('recalculate'),
+      )
+      .with('assetBrlPrivateBonds', 10)
+      .with('assetBrlPublicBonds', 2, (pb) =>
+        pb.with('assetBrlPublicBondBuys', 2).with('assetBrlPublicBondSells', 1),
+      )
+      .create()
   }
 }
